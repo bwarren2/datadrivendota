d313cfe2d979f81f41a1fc5648b97a7482341721
Ben Warren <bwarren2@gmail.com>
2013-09-12 15:51:45 -0400

    Trying new static thing

--- datadrivendota/datadrivendota/s3utils.py
+++ datadrivendota/datadrivendota/s3utils.py
@@ -3,8 +3,8 @@ from storages.backends.s3boto import S3BotoStorage
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaRootS3BotoStorage = lambda: S3BotoStorage(location='media')
 
-from django.contrib.staticfiles.storage import CachedFilesMixin
+from staticfiles.storage import CachedFilesMixin
 from pipeline.storage import PipelineMixin
 
-class S3PipelineStorage(PipelineMixin, CachedFilesMixin, S3BotoStorage):
+class S3PipelineStorage(PipelineMixin, CachedFilesMixin, StaticRootS3BotoStorage):
      pass
