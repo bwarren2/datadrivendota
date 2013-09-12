bd567401a5fac3a59ab6e06980c734dcc6d4a952
Ben Warren <bwarren2@gmail.com>
2013-09-12 15:18:58 -0400

    Removed fake requirements file, trying to install RJSONIO from source

--- init.r
+++ init.r
@@ -1,6 +1,7 @@
 # sudo aptitude install libcurl4-openssl-dev
 # sudo aptitude install libxml2-dev
 #gridSVG
+install.packages("RJSONIO", repos = "http://www.omegahat.org/R", type = "source")
 install.packages("gridSVG",repos='http://cran.rstudio.com/')
 
 #rcharts
