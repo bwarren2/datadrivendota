1740ba5ee87ea20bda531501461a796bfb4c439f
Ben Warren <bwarren2@gmail.com>
2013-09-13 11:18:56 -0400

    Commenting out R installs for this push

--- init.r
+++ init.r
@@ -1,14 +1,14 @@
 # sudo aptitude install libcurl4-openssl-dev
 # sudo aptitude install libxml2-dev
 #gridSVG
-install.packages("RJSONIO", repos = "http://www.omegahat.org/R", type = "source")
-install.packages("gridSVG",repos='http://cran.rstudio.com/')
+#install.packages("RJSONIO", repos = "http://www.omegahat.org/R", type = "source")
+#install.packages("gridSVG",repos='http://cran.rstudio.com/')
 
 #rcharts
-install.packages('devtools',repos='http://cran.rstudio.com/')
-require('devtools')
-install.packages('plyr',repos='http://cran.rstudio.com/')
-install.packages('yaml',repos='http://cran.rstudio.com/')
+#install.packages('devtools',repos='http://cran.rstudio.com/')
+#require('devtools')
+#install.packages('plyr',repos='http://cran.rstudio.com/')
+#install.packages('yaml',repos='http://cran.rstudio.com/')
 
-install_github('rCharts', 'ramnathv')
+#install_github('rCharts', 'ramnathv')
 
