4c017e91e2cfc71d781d9fdc8d27be6778d05510
Ben Warren <bwarren2@gmail.com>
2013-09-17 01:50:19 -0400

    Added a file for the R theme!  Tested in player overview.

---
+++ datadrivendota/datadrivendota/theme.r
@@ -0,0 +1,37 @@
+ddd.theme = list(
+ background = list(col = "#3E1C33"),
+ panel.background = list(col = "#FFBAE9"),
+ add.line = list(col = "black"),
+ add.text = list(col = "black"),
+ plot.polygon = list(col = rainbow(n=7)),
+ box.dot = list(col = "black"),
+ box.rectangle = list(col = "black"),
+ box.umbrella = list(col = "black"),
+ dot.line = list(col = "black"),
+ dot.symbol = list(col = "black"),
+ add.text = list(col = "black"),
+ plot.line = list(col = "black"),
+ plot.symbol = list(col = "black"),
+ reference.line = list(col = "black"),
+ strip.background = list(col = rainbow(n=7)),    #Long
+ strip.shingle = list(col = rainbow(n=7)),       #Long
+ strip.border = list(col = rainbow(n=7)),        #Long
+ par.xlab.text = list(col = "yellow"),
+ par.ylab.text = list(col = "chocolate"),
+ axis.text = list(col = "burlywood"),
+ axis.line = list(col = "goldenrod"),
+ superpose.line = list(col = rainbow(n=7)),      #Long
+ superpose.symbol = list(col = rainbow(n=7)),    #Long
+ superpose.polygon = list(col = rainbow(n=7)),  #Long
+ regions = list(col = c("darkred")),             #Long
+ axis.line = list(col = c("black")),
+ axis.text = list(col = c("black")),
+ box.3d = list(col = c("black")),
+ par.xlab.text = list(col = c("black")),
+ par.ylab.text = list(col = c("black")),
+ par.zlab.text = list(col = c("black")),
+ par.sub.text = list(col = "black"),
+ par.main.text = list(col = "black")
+)
+
+
