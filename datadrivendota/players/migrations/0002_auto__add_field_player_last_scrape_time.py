9f305396af575a7cf1fb1265b117bf3c546fcb54
Ben Warren <bwarren2@gmail.com>
2013-09-30 18:10:42 -0400

    Revamped tasks to be class-based, added last scrape time to pplayer model

---
+++ datadrivendota/players/migrations/0002_auto__add_field_player_last_scrape_time.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Player.last_scrape_time'
+        db.add_column(u'players_player', 'last_scrape_time',
+                      self.gf('django.db.models.fields.IntegerField')(default=0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Player.last_scrape_time'
+        db.delete_column(u'players_player', 'last_scrape_time')
+
+
+    models = {
+        u'players.player': {
+            'Meta': {'object_name': 'Player'},
+            'avatar': ('django.db.models.fields.TextField', [], {}),
+            'avatar_full': ('django.db.models.fields.TextField', [], {}),
+            'avatar_medium': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_scrape_time': ('django.db.models.fields.IntegerField', [], {}),
+            'persona_name': ('django.db.models.fields.TextField', [], {}),
+            'profile_url': ('django.db.models.fields.TextField', [], {}),
+            'steam_id': ('django.db.models.fields.BigIntegerField', [], {'unique': 'True'}),
+            'updated': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['players']
