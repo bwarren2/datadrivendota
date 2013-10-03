a65086b0b0a033229c6392f0f3f4868490320239
Ben Warren <bwarren2@gmail.com>
2013-10-03 00:50:22 -0400

    Testing install

--- init.r
+++ init.r
@@ -2,7 +2,7 @@
 # sudo aptitude install libxml2-dev
 #gridSVG
 #install.packages("RJSONIO", repos = "http://www.omegahat.org/R", type = "source")
-#install.packages("gridSVG",repos='http://cran.rstudio.com/')
+install.packages("gridSVG",repos='http://cran.rstudio.com/')
 
 #rcharts
 #install.packages('devtools',repos='http://cran.rstudio.com/')
