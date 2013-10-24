027a18c8da3f0afe345341d5ffacd24a3dc64add
Ben Warren <bwarren2@gmail.com>
2013-10-24 12:44:50 -0400

    Changed contact link to a mailto

--- datadrivendota/datadrivendota/settings/base.py
+++ datadrivendota/datadrivendota/settings/base.py
@@ -220,7 +220,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
     'social_auth.context_processors.social_auth_by_type_backends',
-    #'datadrivendota.custom_template_context_processors.active',
+    'datadrivendota.custom_template_context_processors.active',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
