77aa2ca5d962d70f3c12c6da2deb5d821e26c050
Ben Warren <bwarren2@gmail.com>
2013-12-19 00:06:13 -0500

    Successful tasks no longer spam their args into logs

--- datadrivendota/matches/management/tasks/valve_api_calls.py
+++ datadrivendota/matches/management/tasks/valve_api_calls.py
@@ -104,7 +104,7 @@ class BaseTask(Task):
         logger.error("Task retry! {args}, {kwargs}, {task_id}".format(args=args,kwargs=kwargs,task_id=task_id))
 
     def on_success(self, retval, task_id, args, kwargs):
-        logger.error("Task success! {args}, {kwargs}, {task_id}".format(args=args,kwargs=kwargs,task_id=task_id))
+        logger.error("Task success! {task_id}".format(task_id=task_id))
 
 class ApiFollower(BaseTask):
 
