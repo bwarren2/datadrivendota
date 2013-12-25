0555b34e497f6808b286bfa5be0a57d588ef491d
Ben Warren <bwarren2@gmail.com>
2013-12-24 23:28:23 -1000

    Testing rCharts install

--- init.r
+++ init.r
@@ -7,9 +7,8 @@ install.packages("RJSONIO", repos = "http://cran.rstudio.com/")
 
 #rcharts
 install.packages('devtools',repos='http://cran.rstudio.com/')
-require('devtools')
 install.packages('plyr',repos='http://cran.rstudio.com/')
 install.packages('yaml',repos='http://cran.rstudio.com/')
 
+require(devtools)
 install_github('rCharts', 'ramnathv')
-
