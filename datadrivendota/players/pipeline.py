35838ea4fa2033b00de49fb482646abe5bf3ddf6
Ben Warren <bwarren2@gmail.com>
2014-01-02 12:01:24 -1000

    Finally creating players on login.  Goddamn exceptions.

--- datadrivendota/players/pipeline.py
+++ datadrivendota/players/pipeline.py
@@ -2,8 +2,7 @@ from players.models import Player
 from django.conf import settings
 
 def create_player(strategy, details, response, *args, **kwargs):
-
-    steam_64_id = kwargs['uid']
+    steam_64_id = int(details['player']['steamid'])
     steam_32_id = steam_64_id % settings.ADDER_32_BIT
     p = Player.objects.get_or_create(steam_id=steam_32_id)
     return {'player_created':p[1]}
