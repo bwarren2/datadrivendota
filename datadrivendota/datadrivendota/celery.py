baf98514f126dd0b7866fea3035b83739c1a9f4e
Ben Warren <bwarren2@gmail.com>
2014-01-14 20:11:18 -1000

    Adding init celery import

--- datadrivendota/datadrivendota/celery.py
+++ datadrivendota/datadrivendota/celery.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from celery import Celery
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datadrivendota.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datadrivendota.settings.production')
 app = Celery('datadrivendota')
 app.config_from_object('celery_config')
 
