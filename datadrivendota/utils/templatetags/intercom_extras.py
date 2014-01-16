513732117bfcc5423dcd8fadee9b525bd64e33d0
Kit La Touche <kit.la.t@gmail.com>
2014-01-15 20:15:56 -0700

    Fixed Intercom.io integration.
    Broken because hmac expects strings, not numbers.

--- datadrivendota/utils/templatetags/intercom_extras.py
+++ datadrivendota/utils/templatetags/intercom_extras.py
@@ -19,6 +19,6 @@ def generate_intercom_user_hash(user_steam_id):
 def intercom_user_hash(user):
     try:
         steam_id = user.userprofile.player.steam_id
-        return generate_intercom_user_hash(steam_id)
+        return generate_intercom_user_hash(str(steam_id))
     except Exception:
         return ""
