256a7ff7194e873845aab138d9e0f65abb1a1097
Ben Warren <bwarren2@gmail.com>
2014-01-26 23:23:24 -0800

    Displaying items for matches and improving item scrape.  Closes 14

--- datadrivendota/items/management/commands/scrapeitemdata.py
+++ datadrivendota/items/management/commands/scrapeitemdata.py
@@ -6,99 +6,148 @@ from json import loads
 from BeautifulSoup import BeautifulSoup
 from urllib2 import urlopen
 from items.models import Item, ItemAttributes, ItemComponents
-
+from collections import defaultdict
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-#        import pdb; pdb.set_trace()
-        id_dict = {}
+        id_dict = defaultdict(dict)
+        edited_json={}
         #Get the item IDs from the local file.
         with open('items.json') as f:
-            id_json = loads(f.read())
-            for item in id_json['items']:
-                id = item['id']
-                name=item['name']
-                id_dict[name]=id
-                i = Item.objects.get_or_create(steam_id=id)[0]
-                i.save()
+            internal_json = loads(f.read())
+            for name, internal_dict in internal_json['DOTAAbilities'].iteritems():
+                name = name[5:]
+                edited_json[name] = internal_dict
+
+        internal_json = edited_json
+
+        for name, information_dict in internal_json.iteritems():
+            id = information_dict['ID']
+
+            id_dict[name]['internal_data'] = information_dict
+            id_dict[name]['id'] = id
+            i = Item.objects.get_or_create(steam_id=id)[0]
+            i.save()
+
         #Get the JSON item data feed.
         #url = 'http://www.dota2.com/jsfeed/itemdata'
         with open('item_detail.json') as f:
-            json = loads(f.read())
+            site_json = loads(f.read())
 
         #Do some cleanup
-        for item in json['itemdata']:
-            for attr in json['itemdata'][item]:
+        for item in site_json['itemdata']:
+            for attr in site_json['itemdata'][item]:
                 if attr in ['attrib','desc']:
                     linelist = []
-                    for line in json['itemdata'][item][attr].split('<br />\n'):
+                    for line in site_json['itemdata'][item][attr].split('<br />\n'):
                         l = ' '.join(BeautifulSoup(line).getText(separator=u' ').split())
                         linelist.append(l.replace("<br />",""))
                     if attr == 'desc':
-                        json['itemdata'][item][attr] = '\n'.join(linelist)
+                        site_json['itemdata'][item][attr] = '\n'.join(linelist)
                     elif attr == 'attrib':
-                        json['itemdata'][item][attr] = linelist
+                        site_json['itemdata'][item][attr] = linelist
                     else:
                         exit("WTF!?")
                 if attr in ['notes']:
-                    json['itemdata'][item][attr] = json['itemdata'][item][attr].replace("<br />","")
+                    site_json['itemdata'][item][attr] = site_json['itemdata'][item][attr].replace("<br />","")
 
-        clean_items={}
-        for item in json['itemdata']:
+        for item in id_dict:
             if item != 'diffusal_blade_2' and item != 'tango_single':
-                id = id_dict[item]
-                clean_items[item] = json['itemdata'][item]
-                clean_items[item]['id']=id
+                try:
+                    id_dict[item]['external_data'] = site_json['itemdata'][item]
+                except KeyError:
+                    print "Could not find external data for {0}".format(item)
 
-#
-        for clean_item in clean_items:
+#        import pdb; pdb.set_trace()
+
+        for clean_item, data_dict in id_dict.iteritems():
             print clean_item
-            item = Item.objects.get(steam_id=clean_items[clean_item]['id'])
-            item.name = clean_items[clean_item]['dname']
-            item.internal_name = clean_item
-            item.quality = clean_items[clean_item]['qual']
-            item.description = clean_items[clean_item]['desc']
-            item.notes = clean_items[clean_item]['notes']
-            item.cost = clean_items[clean_item]['cost']
-            if clean_items[clean_item]['mc'] == 'false':
-                mc = 0
-            else: mc = clean_items[clean_item]['mc']
-            if clean_items[clean_item]['cd'] == 'false':
-                cd = 0
-            else: cd = clean_items[clean_item]['cd']
-            item.mana_cost = mc
-            item.cooldown = cd
-            item.lore = clean_items[clean_item]['lore']
-            item.created = clean_items[clean_item]['created']
-            item.slug_name = slugify(clean_items[clean_item]['dname'])
 
-            url = 'http://media.steampowered.com/apps/dota2/images/items/%s_lg.png' % item
-            lg_image = urlopen(url)
-            tempfile = TemporaryFile()
-            tempfile.write(lg_image.read())
-            tempfile.seek(0)
-            tempfile.read()
-            filename = slugify(item.name)+'_large.png'
-            item.mugshot.save(filename, File(tempfile))
-            url = 'http://media.steampowered.com/apps/dota2/images/items/%s_eg.png' % item
-            eg_image = urlopen(url)
-            tempfile = TemporaryFile()
-            tempfile.write(eg_image.read())
-            tempfile.seek(0)
-            tempfile.read()
-            filename = slugify(item.name)+'_small.png'
-            item.thumbshot.save(filename, File(tempfile))
+            #ugh, bad data conventions.
+            if "dagon" in clean_item:
+                continue
+
+            try:
+                item = Item.objects.get(steam_id=id_dict[clean_item]['id'])
+                item.name = id_dict[clean_item]['external_data']['dname']
+                item.internal_name = clean_item
+                item.quality = id_dict[clean_item]['external_data']['qual']
+                item.description = id_dict[clean_item]['external_data']['desc']
+                item.notes = id_dict[clean_item]['external_data']['notes']
+                item.cost = id_dict[clean_item]['external_data']['cost']
+                if id_dict[clean_item]['external_data']['mc'] == 'false':
+                    mc = 0
+                else: mc = id_dict[clean_item]['external_data']['mc']
+                if id_dict[clean_item]['external_data']['cd'] == 'false':
+                    cd = 0
+                else: cd = id_dict[clean_item]['external_data']['cd']
+                item.mana_cost = mc
+                item.cooldown = cd
+                item.lore = id_dict[clean_item]['external_data']['lore']
+                item.created = id_dict[clean_item]['external_data']['created']
+                item.slug_name = slugify(id_dict[clean_item]['external_data']['dname'])
+
+                url = 'http://media.steampowered.com/apps/dota2/images/items/%s_lg.png' % item
+                lg_image = urlopen(url)
+                tempfile = TemporaryFile()
+                tempfile.write(lg_image.read())
+                tempfile.seek(0)
+                tempfile.read()
+                filename = slugify(item.name)+'_large.png'
+                item.mugshot.save(filename, File(tempfile))
+                url = 'http://media.steampowered.com/apps/dota2/images/items/%s_eg.png' % item
+                eg_image = urlopen(url)
+                tempfile = TemporaryFile()
+                tempfile.write(eg_image.read())
+                tempfile.seek(0)
+                tempfile.read()
+                filename = slugify(item.name)+'_small.png'
+                item.thumbshot.save(filename, File(tempfile))
+
+            except KeyError:
+                item = Item.objects.get(steam_id=id_dict[clean_item]['id'])
+                item.name = slugify(clean_item.replace("_"," ")).title()
+                item.internal_name = clean_item
+                item.slug_name = slugify(item.name)
+
+                if 'recipe' in clean_item:
+                    url = 'http://media.steampowered.com/apps/dota2/images/items/recipe_lg.png'
+                    lg_image = urlopen(url)
+                    tempfile = TemporaryFile()
+                    tempfile.write(lg_image.read())
+                    tempfile.seek(0)
+                    tempfile.read()
+                    filename = slugify(item.name)+'_large.png'
+                    item.mugshot.save(filename, File(tempfile))
+                    url = 'http://media.steampowered.com/apps/dota2/images/items/recipe_lg.png'
+                    eg_image = urlopen(url)
+                    tempfile = TemporaryFile()
+                    tempfile.write(eg_image.read())
+                    tempfile.seek(0)
+                    tempfile.read()
+                    filename = slugify(item.name)+'_small.png'
+                    item.thumbshot.save(filename, File(tempfile))
 
 
-        for clean_item in sorted(clean_items):
-            print "Comp "+ clean_item
-            if clean_items[clean_item]['components'] is not None:
-                for comp in clean_items[clean_item]['components']:
-                    if comp[0:6]!='recipe':
+        for clean_item in sorted(id_dict):
+
+            print "SKIPPING Comp "+ clean_item
+            """
+            try:
+                if id_dict[clean_item]['external_data']['components'] !='null':
+                    for comp in id_dict[clean_item]['external_data']['components']:
                         print comp
                         ItemComponents.objects.get_or_create(product=Item.objects.get(internal_name=clean_item),ingredient=Item.objects.get(internal_name=comp))
-            if clean_items[clean_item]['attrib'] !=['']:
-                for attrib in clean_items[clean_item]['attrib']:
+            except KeyError:
+                pass
+            """
+
+            try:
+                for attrib in id_dict[clean_item]['external_data']['attrib']:
                     ItemAttributes.objects.get_or_create(item=Item.objects.get(internal_name=clean_item),attribute=attrib)
+            except KeyError:
+                pass
+            except Item.DoesNotExist:
+                print clean_item
