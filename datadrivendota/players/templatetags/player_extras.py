74c37c7fd024f3ab3dc8253277b84a9d64696f06
Kit La Touche <kit.la.t@gmail.com>
2014-02-16 17:14:29 -0700

    Display improvements to match summaries.
    Fixes #75.

---
+++ datadrivendota/players/templatetags/player_extras.py
@@ -0,0 +1,24 @@
+from django import template
+from django.core.urlresolvers import reverse
+
+register = template.Library()
+
+
+@register.simple_tag
+def player_link(player):
+    if player.is_masked():
+        return "Anonymous"
+
+    if player.persona_name:
+        text = player.persona_name
+    else:
+        text = str(player.steam_id)
+
+    try:
+        link = reverse(
+            'players:id_detail',
+            kwargs={'player_id': player.steam_id}
+        )
+        return "<a href='{}'>{}</a>".format(link, text)
+    except:
+        return text
