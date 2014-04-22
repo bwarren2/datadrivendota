f2e283b78a3b643622f126ce97d1948ea897a691
Ben Warren <bwarren2@gmail.com>
2014-04-22 19:17:17 -0400

    Removing prints

--- datadrivendota/heroes/views.py
+++ datadrivendota/heroes/views.py
@@ -362,9 +362,6 @@ def hero_performance_api(request):
         hero = Hero.objects.get(name=hero_name)
         game_modes = GameMode.objects.filter(is_competitive=True)
         game_mode_list = [gm.steam_id for gm in game_modes]
-        print '============='
-        print group_var
-        print '============='
 
         datalist, params = hero_performance_chart_json(
             hero=hero.steam_id,
@@ -375,7 +372,6 @@ def hero_performance_api(request):
             group_var=group_var,
             split_var=split_var,
         )
-        print datalist, params
         json_data = outsourceJson(datalist, params)
         response_data = {}
         response_data['result'] = 'success'
