840c882efcafbed76cc136e99f825063ce21b454
Ben Warren <bwarren2@gmail.com>
2014-05-03 23:43:17 -0400

    Moving mode declaration into init

--- datadrivendota/matches/form_fields.py
+++ datadrivendota/matches/form_fields.py
@@ -7,17 +7,15 @@ __all__ = (
 )
 
 
-GAME_MODES = GameMode.objects.filter(visible=True)
-GAME_MODE_CHOICES = [(gm.steam_id, gm.description) for gm in GAME_MODES]
-
-GAME_MODES = GameMode.objects.filter(is_competitive=True, visible=True)
-GAME_MODE_DEFAULTS = [gm.steam_id for gm in GAME_MODES]
-
-
 class MultiGameModeSelect(forms.MultipleChoiceField):
     widget = forms.SelectMultiple(attrs={'class': 'multi-game-mode'})
 
     def __init__(self, *args, **kwargs):
+        GAME_MODES = GameMode.objects.filter(visible=True)
+        GAME_MODE_CHOICES = [(gm.steam_id, gm.description) for gm in GAME_MODES]
+
+        GAME_MODES = GameMode.objects.filter(is_competitive=True, visible=True)
+        GAME_MODE_DEFAULTS = [gm.steam_id for gm in GAME_MODES]
         super(MultiGameModeSelect, self).__init__(*args, **kwargs)
         self.choices = GAME_MODE_CHOICES
         self.initial = GAME_MODE_DEFAULTS
