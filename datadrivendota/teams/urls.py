1ad35796a0ab92b8d0194c050502c133a09d553d
Ben Warren <bwarren2@gmail.com>
2014-06-20 12:39:24 -0400

    Building out team detail

--- datadrivendota/teams/urls.py
+++ datadrivendota/teams/urls.py
@@ -4,4 +4,9 @@ from teams import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.TeamList.as_view(), name='index'),
+    url(
+        r'^(?P<steam_id>[0-9\-]*)/$',
+        views.TeamDetail.as_view(),
+        name='detail'
+    ),
 )
--- datadrivendota/teams/views.py
+++ datadrivendota/teams/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from utils.pagination import SmarterPaginator
 from .models import Team
+from matches.models import Match, PlayerMatchSummary
+from matches.views import annotated_matches
 
 
 class TeamList(ListView):
@@ -10,3 +12,36 @@ class TeamList(ListView):
     def get_queryset(self):
         qs = self.model.objects.all().select_related()
         return qs
+
+
+class TeamDetail(DetailView):
+    """Focusing on a particular team"""
+
+    def get_object(self):
+        return Team.objects.get(steam_id=self.kwargs.get('steam_id'))
+
+    def get_context_data(self, **kwargs):
+        match_list = Match.objects.get(
+            radiant_team_id=self.object.steam_id,
+            skill=4)
+        match_list = match_list.select_related()\
+            .distinct().order_by('-start_time')
+
+        page = self.request.GET.get('page')
+        paginator = SmarterPaginator(
+            object_list=match_list,
+            per_page=15,
+            current_page=page
+        )
+        match_list = paginator.current_page
+
+        pms_list = PlayerMatchSummary.\
+            objects.filter(match__in=match_list)\
+            .select_related().order_by('-match__start_time')[:500]
+        match_data = annotated_matches(pms_list, follow_list)
+
+        context = {
+            'match_list': match_list,
+            'match_data': match_data,
+        }
+        super(TeamDetail, self).get_context_data(**context)
