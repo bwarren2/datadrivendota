734da558ea2fba5b2ac384a61c698a31b2864b57
Ben Warren <bwarren2@gmail.com>
2014-06-27 21:39:07 -0400

    Exclude leagues without games

--- datadrivendota/leagues/managers.py
+++ datadrivendota/leagues/managers.py
@@ -7,5 +7,6 @@ class SortedLeagueManager(models.Manager):
     def get_queryset(self):
         qs = super(SortedLeagueManager, self).get_queryset()\
             .annotate(Max('match__start_time'))\
+            .exclude(match__start_time__max=None)\
             .order_by('-match__start_time__max')
         return qs
