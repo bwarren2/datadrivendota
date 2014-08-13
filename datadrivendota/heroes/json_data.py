25711b2df53630beab0532921f9374c2955e56f1
Ben Warren <bwarren2@gmail.com>
2014-08-13 14:40:46 -0400

    Optimizing progression

--- datadrivendota/heroes/json_data.py
+++ datadrivendota/heroes/json_data.py
@@ -337,13 +337,17 @@ def hero_progression_json(
     pmses = pmses.filter(hero__steam_id=hero, match__validity=Match.LEGIT)
     skill1 = pmses.filter(match__skill=1)\
         .order_by('-match__start_time')[:100]
+
     skill2 = pmses.filter(match__skill=2)\
         .order_by('-match__start_time')[:100]
+
     skill3 = pmses.filter(match__skill=3)\
         .order_by('-match__start_time')[:100]
 
     if player is not None:
-        player_games = pmses.filter(player__steam_id=player)
+        player_games = pmses.filter(player__steam_id=player)\
+            .select_related('match__steam_id')
+
         player_game_ids = [pg.id for pg in player_games]
         pmses_pool = list(chain(skill1, skill2, skill3, player_games))
     else:
@@ -354,7 +358,8 @@ def hero_progression_json(
         requested_pmses = PlayerMatchSummary.objects.filter(
             match__steam_id__in=matches,
             hero__steam_id=hero
-        )
+        ).select_related('match__steam_id')[:100]
+
         pmses_pool.extend(list(requested_pmses))
         requested_ids = [pms.match.steam_id for pms in requested_pmses]
     else:
