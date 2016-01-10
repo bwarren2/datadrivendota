cf56b3e9f830ad73da9bda702315c6ee800c387a
Ben Warren <bwarren2@gmail.com>
2016-01-10 15:15:11 -0500

    Whatever, replay storage gzips stuff with the right headers

--- datadrivendota/datadrivendota/s3utils.py
+++ datadrivendota/datadrivendota/s3utils.py
@@ -11,8 +11,7 @@ from pipeline.storage import PipelineMixin
 
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaRootS3BotoStorage = lambda: S3BotoStorage(location='media')
-ParseS3BotoStorage = lambda: S3BotoStorage(location='processed_replay_parse')
-# ,headers={'Content-Type': 'application/json', 'Content-Encoding': 'gzip'}
+ParseS3BotoStorage = lambda: S3BotoStorage(location='processed_replay_parse',headers={'Content-Type': 'application/json', 'Content-Encoding': 'gzip'}, gzip_content_types=['application/json'], gzip=True)
 
 # # CachedFilesMixin doesn't play well with Boto and S3. It over-quotes things,
 # # causing erratic failures. So we subclass.
