de3b4b0fe7b852e081322ad925341e37002873b7
Ben Warren <bwarren2@gmail.com>
2016-04-12 21:17:54 -0600

    Fix filters

--- datadrivendota/matches/managers.py
+++ datadrivendota/matches/managers.py
@@ -13,6 +13,6 @@ class Unparsed(models.Manager):
 class Parsed(models.Manager):
 
     def get_queryset(self):
-        return super(Parsed, self).get_queryset().filter(
+        return super(Parsed, self).get_queryset().exclude(
             parsed_with=settings.PARSER_VERSION
         )
