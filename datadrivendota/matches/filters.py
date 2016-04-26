557f63f8fbdeea9bea4b331f38687fdb039794c8
Kit La Touche <kit@transneptune.net>
2016-04-26 17:31:51 -0600

    Make match filter fuzzier.
    Case insensitive, and partial matches allowed.

--- datadrivendota/matches/filters.py
+++ datadrivendota/matches/filters.py
@@ -20,12 +20,12 @@ class MatchFilter(filters.FilterSet):
             hero = self.get_associated_value('hero')
             if hero:
                 queryset = queryset.filter(
-                    playermatchsummary__player__persona_name=value,
-                    playermatchsummary__hero__name=hero,
+                    playermatchsummary__player__persona_name__icontains=value,
+                    playermatchsummary__hero__name__icontains=hero,
                 )
             else:
                 queryset = queryset.filter(
-                    playermatchsummary__player__persona_name=value,
+                    playermatchsummary__player__persona_name__icontains=value,
                 )
         return queryset
 
@@ -34,23 +34,26 @@ class MatchFilter(filters.FilterSet):
             player = self.get_associated_value('player')
             if player:
                 queryset = queryset.filter(
-                    playermatchsummary__player__persona_name=player,
-                    playermatchsummary__hero__name=value,
+                    playermatchsummary__player__persona_name__icontains=player,
+                    playermatchsummary__hero__name__icontains=value,
                 )
             else:
                 queryset = queryset.filter(
-                    playermatchsummary__hero__name=value,
+                    playermatchsummary__hero__name__icontains=value,
                 )
         return queryset
 
     league = django_filters.CharFilter(
         name='league__name',
+        lookup_expr='icontains',
     )
     radiant_team = django_filters.CharFilter(
         name='radiant_team__name',
+        lookup_expr='icontains',
     )
     dire_team = django_filters.CharFilter(
         name='dire_team__name',
+        lookup_expr='icontains',
     )
     start_date = django_filters.MethodFilter()
     end_date = django_filters.MethodFilter()
