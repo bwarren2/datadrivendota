511b45d8d60547d88ae84288cfe9c8eb3c4eafbf
Ben Warren <bwarren2@gmail.com>
2016-05-17 13:21:14 -0600

    hotfix import link name

--- datadrivendota/parserpipe/urls.py
+++ datadrivendota/parserpipe/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^dash/$', views.DashView.as_view(), name='dash'),
-    url(r'^import/$', views.MatchRequestView.as_view(), name='import'),
+    url(r'^import/$', views.MatchRequestView.as_view(), name='user-import'),
     url(r'^ajax-tasks/$', views.TasksView.as_view(), name='tasks'),
     url(
         r'^ajax-import/$',
