156ec97a00044cca37ed314a6b4c08992bb76afd
Ben Warren <bwarren2@gmail.com>
2017-05-17 09:01:56 -0400

    Acme 2

--- datadrivendota/datadrivendota/urls.py
+++ datadrivendota/datadrivendota/urls.py
@@ -43,6 +43,12 @@ urlpatterns = [
             content_type='text/plain'
         ),
         name='acme'),
+    url(r'^.well-known/acme-challenge/lTZrfdiq9JncXt_2hLv75FKQx2N2yDjdw9cVjaFdYcA$',
+        TemplateView.as_view(
+            template_name='acme2.txt',
+            content_type='text/plain'
+        ),
+        name='acme'),
     url(
         r'^api/combobox_tags/$',
         views.ComboboxAjaxView.as_view(),
